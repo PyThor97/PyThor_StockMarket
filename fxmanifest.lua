77dcc4b0d24846569fb41b57dfe8647a058c2c2e
PyThor97 <pythor97il@gmail.com>
2025-01-03 21:39:20 +0200

    using vorp version control

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,6 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 game { 'rdr3' }
 lua54 'yes'
 
-version '2.0'
 name 'PyThor_StockMarket'
 author 'PyThor'
 description 'In depth stock market system with missions'
@@ -26,3 +25,8 @@ dependencies {
     'bcc-utils',
     'feather-menu'
 }
+
+version '2.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/PyThor97/PyThor_StockMarket'
